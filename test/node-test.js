ceed94d3ce2e98f450b74e74a25d2ffd00bccaf4
TimZhang <zewenzhang@gmail.com>
2015-01-06 11:57:13 +0800

    Update node-test.js

--- test/node-test.js
+++ test/node-test.js
@@ -4,7 +4,7 @@ var a = Class({name: 'Tim', printName: function() {console.log(this.name);}});
 
 var b = a.extend();
 
-ob = new b();
+var ob = new b();
 console.log(ob.name);
 a.prototype.name = 'Zhang'
 console.log(ob.name);
